166e6e862166a29fc4664563097c1cca8a013089
Shannon Yu <shannon.qyu@gmail.com>
2024-05-21 16:27:10 +0100

    new

--- code/sleemory_localiser/test.py
+++ code/sleemory_localiser/test.py
@@ -1,10 +1,15 @@
 import numpy as np
-layer = 'conv5'
 
+# sleemory_retrieval
+data = np.load(f'output/sleemory_retrieval/test_pred_eeg/pred_eeg_with_1000feats.npy', 
+               allow_pickle=True).item()
+for key, value in data.items():
+    print(key, value.shape)
+print('')
+
+# sleemory_localiser
+layer = 'conv5'
 data = np.load(f'output/sleemory_localiser/test_pred_eeg/{layer}_eeg.npy', 
                allow_pickle=True).item()
-print(data['train_eeg'].shape, data['test_eeg'].shape, data['pred_eeg'].shape)
-
-data = np.load(f'output/sleemory_retrieval/test_pred_eeg/{layer}_eeg.npy', 
-               allow_pickle=True).item()
-print(data['pred_eeg'].shape)
+for key, value in data.items():
+    print(layer, key, value.shape)
