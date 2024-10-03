bb1ccf997c7937e36952d2b2f96bc998d088518f
Simon Faghel-Soubeyrand <simonsoubeyrand@gmail.com>
2024-10-03 16:12:58 +0100

    new

--- code/vox/run_all.py
+++ code/vox/run_all.py
@@ -1,13 +1,15 @@
 import os
 
 networks = 'ResNet'
-layers = ['maxpool', 'layer3']
+layers = [# 'maxpool', 
+          'layer3']
 # layers = ['conv1', 'conv2', 'conv3', 'conv4', 'conv5', 'fc6', 'fc7', 'fc8','all']
 
 subs = range(6, 27)
-for layer in layers:
-    for sub in subs:
-        if sub != 17:
+
+for sub in subs:
+    if sub != 17:
+        for layer in layers:
             os.system(f'python3 code/vox/model_ridge_fracs_PCA.py --sub {sub} --networks {networks} --layer_name {layer}')
 
 # for sub in subs:
