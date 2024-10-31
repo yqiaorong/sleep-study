d026792efffd1a7953c48fc2d2ae40588d6139ae
Shannon Yu <shannon.qyu@gmail.com>
2024-10-31 19:26:32 +0000

    clean

--- code/archive/fmaps/resnext.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-model = torch.hub.load('facebookresearch/WSL-Images', 'resnext101_32x8d_wsl')
-
-for name, layer in model.named_modules():
-    print(name)
--- code/archive/sleemory_retrieval/corr_all.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for sub in range(4, 26):
-    if sub == 17:
-        pass
-    else:
-        os.system(f'python code/sleemory_retrieval/corr_whitenFalse.py --sub {sub}')
