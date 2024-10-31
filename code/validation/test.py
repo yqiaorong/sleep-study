58cd9cba59fdb6c98c7ca62cdea03a6b8e39490c
Shannon Yu <shannon.qyu@gmail.com>
2024-10-31 19:28:31 +0000

    new

--- code/validation/test.py
+++
@@ -1,6 +0,0 @@
-save_dir = f'output/sleemory_localiser_vox/permutation_test/'
-import scipy
-
-for i in range(5):
-    data = scipy.io.loadmat(f'{save_dir}/ResNet-layer3_corr_chunk-{i}.mat')
-    print(data['end_sub'])
